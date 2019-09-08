c5a393b389717da81cd409f441f2a3f10103a657
Leon <leon_lts@hotmail.com>
2019-09-08 10:29:26 -0300

    commit tip

--- job_adventure/lib/main.dart
+++ job_adventure/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:job_adventure/Screens/LoginScreen.dart';
 void main() => runApp(MaterialApp(
   title: 'Job Adventure',
   initialRoute: 'loginScreen',
+  debugShowCheckedModeBanner: false,
   routes:{
     'loginScreen': (context) => LoginScreen(),
     'MainMenu': (context) => NavigationMenu()
