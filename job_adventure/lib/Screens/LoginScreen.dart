ba8bfc09ad5a30a295c7216854628faa03f64b6d
Lucas Vaz <lucassvaz@yahoo.com.br>
2019-09-09 17:11:02 -0300

    gitflow-feature-stash: Login

--- job_adventure/lib/Screens/LoginScreen.dart
+++ job_adventure/lib/Screens/LoginScreen.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'package:html/parser.dart' show parse;
+//import 'package:html/dom.dart';
+
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+const String ACCEPT = 'https://trello.com/1/token/approve';
+const String URL = "https://trello.com/1/authorize?expiration=never&name=Job+Adventure&scope=read%2Cwrite&response_type=token&key=57a893b02ea2046b82ac861766a34bed";
+const String API_KEY = "57a893b02ea2046b82ac861766a34bed";
+const String API_SECRET = "e69cc4919f2196a1318d23981e2389b7ba8ca7425eb7fb1a9cc85862687c4a9f";
 
 class LoginScreen extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+    final webview = FlutterWebviewPlugin();
+
+
+
     return Scaffold(
       body: ListView(
         children: [
@@ -32,9 +49,12 @@ class LoginScreen extends StatelessWidget {
           Padding( // Login button
             padding: EdgeInsets.only(top: 30, left: 100, right: 100),
             child: RaisedButton(
-              onPressed: () {Navigator.pushNamed(context, 'MainMenu');}, // Goes to the main menu when pressed, change when implementing the DB
               color: Color.fromRGBO(255, 211, 109, 0.4),
               child: Text('Login',style: TextStyle(fontSize: 20)),
+              onPressed: () {
+                Navigator.of(context).pushNamed("/webview");
+                _ackAlert(context);
+              },
             )
           ),
           Padding( // Register button
@@ -46,6 +66,56 @@ class LoginScreen extends StatelessWidget {
           )
         ]
       )
-    ); 
+    );
   }
+
+
+
+  void _authorise() async {
+    http.Client client = new http.Client();
+
+    http.Response response = await client.post(URL);
+    print(response.body);
+
+  }
+
+  Future<void> _ackAlert(BuildContext context) {
+
+    String txt;
+
+    /*if (token != null){
+      txt = token.accessToken;
+    } else {
+      txt = 'NULL';
+    }*/
+
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Token Test'),
+          content: Text('Placeholder'),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Ok'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void getKey (FlutterWebviewPlugin webview) {
+    webview.getUrl()
+    webview.onUrlChanged.listen((String url) {
+      if (url == ACCEPT){
+        var response = http.get(ACCEPT);
+        print(response.body);
+      }
+    });
+  }
+
 }
--- job_adventure/lib/main.dart
+++ job_adventure/lib/main.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:job_adventure/Screens/NavigationMenu.dart';
 import 'package:job_adventure/Screens/LoginScreen.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+const String URL = "https://trello.com/1/authorize?expiration=never&name=Job+Adventure&scope=read%2Cwrite&response_type=token&key=57a893b02ea2046b82ac861766a34bed";
+
 
 void main() => runApp(MaterialApp(
   title: 'Job Adventure',
   initialRoute: 'loginScreen',
   routes:{
     'loginScreen': (context) => LoginScreen(),
-    'MainMenu': (context) => NavigationMenu()
-  }
+    'MainMenu': (context) => NavigationMenu(),
+    "/webview": (_) => WebviewScaffold(
+      url: URL,
+      appBar: AppBar(
+        title: Text("Login with Trello"),
+      ),
+      withJavascript: true,
+      withLocalStorage: true,
+      withZoom: true,
+    )
+  },
 ));
 
 
