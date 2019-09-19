1b7712147effa5899404865d29a2da4edc758413
Vlademir Celso dos Santos Junior <vladecsjr@gmail.com>
2019-09-19 12:13:22 -0300

    Auto stash before revert of "Merge remote-tracking branch 'origin/trelloConnectionDone'"

--- job_adventure/lib/main.dart
+++ job_adventure/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:job_adventure/Screens/NavigationMenu.dart';
 import 'package:job_adventure/Screens/LoginScreen.dart';
 import 'package:job_adventure/Screens/QuestPage.dart';
 
+
 void main() => runApp(MaterialApp(
   title: 'Job Adventure',
   initialRoute: 'loginScreen',
